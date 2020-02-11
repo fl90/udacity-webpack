8e56411b94136f18be7b437a2e98fcf2d7303f52
FlorianLiesenfeld <florian.liesenfeld@247grad.de>
2020-02-11 12:44:32 +0100

    Fixing Functionality

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,20 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
                 test: '/\.js/$',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
